a7c40f1726c1e40718b9f10b7e64115794807b48
itay levi <eitay.levi@gmail.com>
2019-11-20 02:34:59 +0200

    added data length encoding

--- EE Final Project/src/DisplayEncoder.java
+++ EE Final Project/src/DisplayEncoder.java
@@ -1,48 +1,101 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
-
 import javax.imageio.ImageIO;
 import java.io.IOException;
 
 
+enum ROW {
+	  WITH_POS_DET,
+	  WITHOUT_POS_DET,
+	  SAME_ROW
+	}
+
 public class DisplayEncoder {
 	
-	public final static int MODULES_IN_ENCODED_IMAGE_DIM = 700;
-	public final static int MODULES_IN_MARGIN = 2;
-	public final static int PIXELS_IN_MODULE = 3;
-	public final static int BLACK = 0xFF000000;
-	public final static int WHITE = 0x0000000;
+	final static int MODULES_IN_ENCODED_IMAGE_DIM = 700;
+	final static int MODULES_IN_MARGIN = 2;
+	final static int MODULES_IN_POS_DET = 7; //maybe change to 8 due to white margins from inside of position detector and draw them in position detector creation
+	final static int PIXELS_IN_MODULE = 3;
+	final static int BLACK = 0xFF000000;
+	final static int WHITE = 0x0000000;
+	final static int DATA_LEN_ENCODING_LENGTH = 20;
 	
-	public static void main(String... args) throws IOException {
-		byte[] testData = new byte[10];
+	public static void main(String... args) throws Exception {
+		String testData = "BLABLAa";
 		encodeBytes(testData);
 	}
 	
-	public static BufferedImage encodeBytes(byte[] binaryData) {
+	public static BufferedImage encodeBytes(String binaryData) throws Exception {
 		
 		//allocate space including white margins
 		BufferedImage image = new BufferedImage(MODULES_IN_ENCODED_IMAGE_DIM*PIXELS_IN_MODULE,
-				MODULES_IN_ENCODED_IMAGE_DIM*PIXELS_IN_MODULE, BufferedImage.TYPE_INT_ARGB);
-		//create margins
+				MODULES_IN_ENCODED_IMAGE_DIM*PIXELS_IN_MODULE, BufferedImage.TYPE_INT_ARGB);		 
+		// Clear the background with white
+		Graphics2D g = (Graphics2D) image.getGraphics();
+		g.setBackground(Color.WHITE);
+		g.clearRect(0, 0, MODULES_IN_ENCODED_IMAGE_DIM*PIXELS_IN_MODULE, MODULES_IN_ENCODED_IMAGE_DIM*PIXELS_IN_MODULE);
 		//create position detector
 		createPositionDetectors(image);
 		//encode data length - 20 bits - masked
+		encodeDataLen(image, binaryData.length());
 		//encode data - masked
 		
+		//print image for testing
 		File newPathQr = new File("C:\\Users\\user\\Downloads\\new qrcode.png");
-		try {
-			ImageIO.write(image, "png", newPathQr);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		ImageIO.write(image, "png", newPathQr);
+
+		return image;
+	}
+
+
+	private static void encodeDataLen(BufferedImage image, int length) throws Exception {
+		//LSB is encoded as the first (leftmost) bit
+		int i;
+		int row = MODULES_IN_MARGIN * PIXELS_IN_MODULE;
+		int col = (MODULES_IN_MARGIN + MODULES_IN_POS_DET) * PIXELS_IN_MODULE;
+		final int MASK = 1;	
+		
+		if(length>0xFFFFF)
+			throw new Exception("Data is too large to be encoded!");
+		
+		for(i = 0; i < DATA_LEN_ENCODING_LENGTH; i++) {
+			if((MASK&length) == 1)
+				colorModule(image, BLACK, row, col);
+			col+= PIXELS_IN_MODULE;
+			//maybe remove the following because this will probably wont be the situation
+			switch(checkForColumnEnd(row, col)){
+			case WITH_POS_DET:
+				col = (MODULES_IN_MARGIN + MODULES_IN_POS_DET) * PIXELS_IN_MODULE;
+				row+= PIXELS_IN_MODULE;
+				break;
+			case WITHOUT_POS_DET:
+				col = 0;
+				row+= PIXELS_IN_MODULE;	
+				break;
+			default:
+				break;				
+			}
+			length = length>>1;
 		}
 		
-		return image;
+	}
+
+	private static ROW checkForColumnEnd(int row, int col) {
+		if(col == (MODULES_IN_ENCODED_IMAGE_DIM - MODULES_IN_MARGIN - MODULES_IN_POS_DET) * PIXELS_IN_MODULE &&
+				(row < (MODULES_IN_MARGIN + MODULES_IN_POS_DET) * PIXELS_IN_MODULE || 
+						row > (MODULES_IN_ENCODED_IMAGE_DIM - MODULES_IN_MARGIN - MODULES_IN_POS_DET) * PIXELS_IN_MODULE) ) {
+			return ROW.WITH_POS_DET;
+		}
+		else if(col == (MODULES_IN_ENCODED_IMAGE_DIM - MODULES_IN_MARGIN) * PIXELS_IN_MODULE) {
+			return ROW.WITHOUT_POS_DET;
+		}
+		return ROW.SAME_ROW;
 	}
 
 	private static void createPositionDetectors(BufferedImage image) {
 		
-		final int MODULES_IN_POS_DET = 7;
 		final int MID_LAYER_OFFSET_1 = 1; final int MID_LAYER_OFFSET_2 = 5;
 		
 		int rowTopLeft = MODULES_IN_MARGIN * PIXELS_IN_MODULE;
@@ -53,28 +106,13 @@ public class DisplayEncoder {
 		int colBottomLeft = MODULES_IN_MARGIN * PIXELS_IN_MODULE;
 		int rowModuleOffset, colModuleOffset;
 		
-		for(rowModuleOffset = 0;rowModuleOffset<  MODULES_IN_POS_DET; rowModuleOffset++) {
-			for(colModuleOffset = 0;colModuleOffset<  MODULES_IN_POS_DET; colModuleOffset++) {
+		for(rowModuleOffset = 0; rowModuleOffset< MODULES_IN_POS_DET; rowModuleOffset++) {
+			for(colModuleOffset = 0; colModuleOffset< MODULES_IN_POS_DET; colModuleOffset++) {
 				
-				if( (rowModuleOffset == MID_LAYER_OFFSET_1 || rowModuleOffset == MID_LAYER_OFFSET_2) &&
-						(colModuleOffset >= MID_LAYER_OFFSET_1 && colModuleOffset <= MID_LAYER_OFFSET_2)) {
-					colorModule(image, WHITE, rowTopLeft + rowModuleOffset * PIXELS_IN_MODULE,
-							colTopLeft + colModuleOffset * PIXELS_IN_MODULE);
-					colorModule(image, WHITE, rowTopRight + rowModuleOffset * PIXELS_IN_MODULE,
-							colTopRight + colModuleOffset * PIXELS_IN_MODULE);
-					colorModule(image, WHITE, rowBottomLeft + rowModuleOffset * PIXELS_IN_MODULE,
-							colBottomLeft + colModuleOffset * PIXELS_IN_MODULE);
-				}
-				else if( (rowModuleOffset > MID_LAYER_OFFSET_1 && rowModuleOffset < MID_LAYER_OFFSET_2) &&
-						(colModuleOffset == MID_LAYER_OFFSET_1 || colModuleOffset == MID_LAYER_OFFSET_2)) {
-					colorModule(image, WHITE, rowTopLeft + rowModuleOffset * PIXELS_IN_MODULE,
-							colTopLeft + colModuleOffset * PIXELS_IN_MODULE);	
-					colorModule(image, WHITE, rowTopRight + rowModuleOffset * PIXELS_IN_MODULE,
-							colTopRight + colModuleOffset * PIXELS_IN_MODULE);
-					colorModule(image, WHITE, rowBottomLeft + rowModuleOffset * PIXELS_IN_MODULE,
-							colBottomLeft + colModuleOffset * PIXELS_IN_MODULE);
-				}
-				else {
+				if( !((rowModuleOffset == MID_LAYER_OFFSET_1 || rowModuleOffset == MID_LAYER_OFFSET_2) &&
+						(colModuleOffset >= MID_LAYER_OFFSET_1 && colModuleOffset <= MID_LAYER_OFFSET_2)) &&
+						!((rowModuleOffset > MID_LAYER_OFFSET_1 && rowModuleOffset < MID_LAYER_OFFSET_2) &&
+								(colModuleOffset == MID_LAYER_OFFSET_1 || colModuleOffset == MID_LAYER_OFFSET_2))) {
 					colorModule(image, BLACK, rowTopLeft + rowModuleOffset * PIXELS_IN_MODULE,
 							colTopLeft + colModuleOffset * PIXELS_IN_MODULE);
 					colorModule(image, BLACK, rowTopRight + rowModuleOffset * PIXELS_IN_MODULE,
