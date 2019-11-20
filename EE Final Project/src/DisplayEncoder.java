f6dec00755fc52f476821a0fa6244b494047bf2a
itay levi <eitay.levi@gmail.com>
2019-11-20 12:08:49 +0200

    Position class created. Module filling using Graphics2D.

--- EE Final Project/src/DisplayEncoder.java
+++ EE Final Project/src/DisplayEncoder.java
@@ -3,7 +3,6 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
-import java.io.IOException;
 
 
 enum ROW {
@@ -18,8 +17,8 @@ public class DisplayEncoder {
 	final static int MODULES_IN_MARGIN = 2;
 	final static int MODULES_IN_POS_DET = 7; //maybe change to 8 due to white margins from inside of position detector and draw them in position detector creation
 	final static int PIXELS_IN_MODULE = 3;
-	final static int BLACK = 0xFF000000;
-	final static int WHITE = 0x0000000;
+	//final static int BLACK = 0xFF000000;
+	//final static int WHITE = 0x0000000;
 	final static int DATA_LEN_ENCODING_LENGTH = 20;
 	
 	public static void main(String... args) throws Exception {
@@ -36,10 +35,11 @@ public class DisplayEncoder {
 		Graphics2D g = (Graphics2D) image.getGraphics();
 		g.setBackground(Color.WHITE);
 		g.clearRect(0, 0, MODULES_IN_ENCODED_IMAGE_DIM*PIXELS_IN_MODULE, MODULES_IN_ENCODED_IMAGE_DIM*PIXELS_IN_MODULE);
+		g.setColor(Color.BLACK);
 		//create position detector
-		createPositionDetectors(image);
+		createPositionDetectors(image, g);
 		//encode data length - 20 bits - masked
-		encodeDataLen(image, binaryData.length());
+		encodeDataLen(image, g, binaryData.length());
 		//encode data - masked
 		
 		//print image for testing
@@ -50,51 +50,55 @@ public class DisplayEncoder {
 	}
 
 
-	private static void encodeDataLen(BufferedImage image, int length) throws Exception {
+	private static void encodeDataLen(BufferedImage image, Graphics2D g, int length) throws Exception {
 		//LSB is encoded as the first (leftmost) bit
 		int i;
-		int row = MODULES_IN_MARGIN * PIXELS_IN_MODULE;
-		int col = (MODULES_IN_MARGIN + MODULES_IN_POS_DET) * PIXELS_IN_MODULE;
-		final int MASK = 1;	
+		boolean currBitIs1;
+		Position p = new Position();
+		p.row = MODULES_IN_MARGIN * PIXELS_IN_MODULE;
+		p.col = (MODULES_IN_MARGIN + MODULES_IN_POS_DET) * PIXELS_IN_MODULE;
+		p.colModul = MODULES_IN_MARGIN + MODULES_IN_POS_DET;
+		final int LSB_MASK = 1;	
 		
 		if(length>0xFFFFF)
 			throw new Exception("Data is too large to be encoded!");
 		
 		for(i = 0; i < DATA_LEN_ENCODING_LENGTH; i++) {
-			if((MASK&length) == 1)
-				colorModule(image, BLACK, row, col);
-			col+= PIXELS_IN_MODULE;
+			currBitIs1 = (LSB_MASK&length) == 1;
+			encodeBit(image, g, p, currBitIs1);			
 			//maybe remove the following because this will probably wont be the situation
-			switch(checkForColumnEnd(row, col)){
-			case WITH_POS_DET:
-				col = (MODULES_IN_MARGIN + MODULES_IN_POS_DET) * PIXELS_IN_MODULE;
-				row+= PIXELS_IN_MODULE;
-				break;
-			case WITHOUT_POS_DET:
-				col = 0;
-				row+= PIXELS_IN_MODULE;	
-				break;
-			default:
-				break;				
-			}
+			checkForColumnEnd(p);
 			length = length>>1;
 		}
 		
 	}
 
-	private static ROW checkForColumnEnd(int row, int col) {
-		if(col == (MODULES_IN_ENCODED_IMAGE_DIM - MODULES_IN_MARGIN - MODULES_IN_POS_DET) * PIXELS_IN_MODULE &&
-				(row < (MODULES_IN_MARGIN + MODULES_IN_POS_DET) * PIXELS_IN_MODULE || 
-						row > (MODULES_IN_ENCODED_IMAGE_DIM - MODULES_IN_MARGIN - MODULES_IN_POS_DET) * PIXELS_IN_MODULE) ) {
-			return ROW.WITH_POS_DET;
-		}
-		else if(col == (MODULES_IN_ENCODED_IMAGE_DIM - MODULES_IN_MARGIN) * PIXELS_IN_MODULE) {
-			return ROW.WITHOUT_POS_DET;
-		}
-		return ROW.SAME_ROW;
+
+	private static void encodeBit(BufferedImage image, Graphics2D g, Position p, boolean currBitIs1) {
+
+		boolean toggleBit;
+		
+		toggleBit = p.colModul%3 == 0;
+		p.colModul++;
+		if(currBitIs1 ^ toggleBit)
+			g.fillRect(p.col, p.row, PIXELS_IN_MODULE, PIXELS_IN_MODULE);
+		p.col+= PIXELS_IN_MODULE;
 	}
 
-	private static void createPositionDetectors(BufferedImage image) {
+	private static void checkForColumnEnd(Position p) {
+		if(p.col == (MODULES_IN_ENCODED_IMAGE_DIM - MODULES_IN_MARGIN - MODULES_IN_POS_DET) * PIXELS_IN_MODULE &&
+				(p.row < (MODULES_IN_MARGIN + MODULES_IN_POS_DET) * PIXELS_IN_MODULE || 
+						p.row > (MODULES_IN_ENCODED_IMAGE_DIM - MODULES_IN_MARGIN - MODULES_IN_POS_DET) * PIXELS_IN_MODULE) ) {
+			p.col = (MODULES_IN_MARGIN + MODULES_IN_POS_DET) * PIXELS_IN_MODULE;
+			p.row+= PIXELS_IN_MODULE;
+		}
+		else if(p.col == (MODULES_IN_ENCODED_IMAGE_DIM - MODULES_IN_MARGIN) * PIXELS_IN_MODULE) {
+			p.col = 0;
+			p.row+= PIXELS_IN_MODULE;
+		}
+	}
+
+	private static void createPositionDetectors(BufferedImage image, Graphics2D g) {
 		
 		final int MID_LAYER_OFFSET_1 = 1; final int MID_LAYER_OFFSET_2 = 5;
 		
@@ -113,26 +117,22 @@ public class DisplayEncoder {
 						(colModuleOffset >= MID_LAYER_OFFSET_1 && colModuleOffset <= MID_LAYER_OFFSET_2)) &&
 						!((rowModuleOffset > MID_LAYER_OFFSET_1 && rowModuleOffset < MID_LAYER_OFFSET_2) &&
 								(colModuleOffset == MID_LAYER_OFFSET_1 || colModuleOffset == MID_LAYER_OFFSET_2))) {
-					colorModule(image, BLACK, rowTopLeft + rowModuleOffset * PIXELS_IN_MODULE,
-							colTopLeft + colModuleOffset * PIXELS_IN_MODULE);
-					colorModule(image, BLACK, rowTopRight + rowModuleOffset * PIXELS_IN_MODULE,
-							colTopRight + colModuleOffset * PIXELS_IN_MODULE);
-					colorModule(image, BLACK, rowBottomLeft + rowModuleOffset * PIXELS_IN_MODULE,
-							colBottomLeft + colModuleOffset * PIXELS_IN_MODULE);
+					g.fillRect(colTopLeft + colModuleOffset * PIXELS_IN_MODULE,
+							rowTopLeft + rowModuleOffset * PIXELS_IN_MODULE, PIXELS_IN_MODULE, PIXELS_IN_MODULE);
+					g.fillRect(colTopRight + colModuleOffset * PIXELS_IN_MODULE,
+							 rowTopRight + rowModuleOffset * PIXELS_IN_MODULE, PIXELS_IN_MODULE, PIXELS_IN_MODULE);
+					g.fillRect(colBottomLeft + colModuleOffset * PIXELS_IN_MODULE,
+							rowBottomLeft + rowModuleOffset * PIXELS_IN_MODULE, PIXELS_IN_MODULE, PIXELS_IN_MODULE);
 				}
 			}
 		}		
-	}
-
-	private static void colorModule(BufferedImage image, int color, int topRowInd, int leftColInd) {
-
-		int rowOffset, colOffset;
-		
-		for(rowOffset = 0; rowOffset < PIXELS_IN_MODULE; rowOffset++) {
-			for(colOffset = 0; colOffset < PIXELS_IN_MODULE; colOffset++) {
-				image.setRGB(leftColInd + colOffset, topRowInd + rowOffset, color);
-			}
-		}
-	}
+	}	
 	
+}
+
+class Position{
+	int row;
+	int col;
+	//int rowModul;
+	int colModul;
 }
