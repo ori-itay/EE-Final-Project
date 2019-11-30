c4f79e6d839c0ed38043f3b2b1fce05aa2eb5fb0
itay levi <eitay.levi@gmail.com>
2019-11-30 12:18:14 +0200

    decoding rotated image 0,90,180,270 works

--- pc/src/QrImage.java
+++ pc/src/QrImage.java
@@ -68,9 +68,14 @@ class RotatedImageSampler extends StdImageSampler {
 	int rotationCounterClockwise = 0;//by degrees
 	
 	public int getPixel(int rowPixel, int colPixel) {
-		int newRow = (int) (-Math.sin(rotationCounterClockwise)*colPixel + Math.cos(rotationCounterClockwise)*rowPixel);
-		int newCol = (int) (Math.cos(rotationCounterClockwise)*colPixel + Math.sin(rotationCounterClockwise)*rowPixel);
-		return this.pixelMatrix[newRow][newCol];
+		if (rotationCounterClockwise == 0)
+			return pixelMatrix[rowPixel][colPixel];
+		if (rotationCounterClockwise == 90)
+			return pixelMatrix[colPixel][pixelMatrix.length-1-rowPixel];
+		if (rotationCounterClockwise == 180)
+			return pixelMatrix[pixelMatrix.length-1-rowPixel][pixelMatrix.length-1-colPixel];
+		return pixelMatrix[pixelMatrix.length-1-colPixel][rowPixel]; //270 rotation
+		
 	}
 	
 }
