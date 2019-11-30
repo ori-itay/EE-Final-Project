35d09ba318ec9dac9da820b3452064b9e16ea967
ori glassman <origlassman@mail.tau.ac.il>
2019-11-30 12:52:41 +0200

    App now asks for permissions + displays QR code image from  file located in /Downloads

--- mobile/VisualCrypto/app/src/main/java/com/example/visualcrypto/MainActivity.java
+++ mobile/VisualCrypto/app/src/main/java/com/example/visualcrypto/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.visualcrypto;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -21,12 +25,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getImageFromFile(View view){
-        //ContextCompat.checkSelfPermission(MainActivity.Manifest.)
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED){
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+            }
+        }
+
         File imgFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "qrcode.png");
         if (imgFile.exists()){
             Bitmap bMap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
 
-            ImageView iView = (ImageView) findViewById(R.id.iView);
+
+            ImageView iView = (ImageView) findViewById(R.id.imgDisplay); // fails here
             iView.setImageBitmap(bMap);
         }
     }
